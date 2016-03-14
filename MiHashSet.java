376db3708f031333f37ac7c739452d9e94f27f8f
KevinFN04 <kevin.felix.04@gmail.com>
2016-03-14 12:27:27 +0100

    añadido metodo para añadir un valor a la lista

---
+++ MiHashSet.java
@@ -0,0 +1,35 @@
+import java.util.*;
+/**
+ * Write a description of class MiHashSet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MiHashSet
+{
+    private ArrayListInt lista;
+
+    /**
+     * Constructor for objects of class MiHashSet
+     */
+    public MiHashSet()
+    {
+        lista = new ArrayListInt();
+    }
+
+    /**
+     * Añade un valor a la lista de valores
+     */
+    public boolean add(int valor)
+    {
+        boolean noContiene = false;
+        if (!lista.contains(valor)){
+            lista.add(valor);
+        }
+        else{
+            noContiene = true;
+        }
+        return noContiene;
+    }
+
+}
